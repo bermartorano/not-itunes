472d86c31b7b165e42a5247529e3dc57f9ae8003
Bernardo Martorano <bernardormartorano@gmail.com>
2022-11-07 14:50:28 -0300

    Requisito 08 concluído mas com alguns erros

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends Component {
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl, trackId, handleClickFavorites, favorite } = this.props;
 
     return (
       <div>
@@ -13,6 +13,10 @@ class MusicCard extends Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
+        <label htmlFor={ `${trackId}` } data-testid={ `checkbox-music-${trackId}` }>
+          Favorita
+          <input id={ `${trackId}` } type="checkbox" onClick={ handleClickFavorites } checked={ favorite } />
+        </label>
       </div>
     );
   }
@@ -23,9 +27,15 @@ export default MusicCard;
 MusicCard.propTypes = {
   trackName: PropTypes.string,
   previewUrl: PropTypes.string,
+  trackId: PropTypes.number,
+  handleClickFavorites: PropTypes.func,
+  favorite: PropTypes.bool,
 };
 
 MusicCard.defaultProps = {
   trackName: 'Não recebi o nome',
   previewUrl: 'Não recebi a url',
+  trackId: 0,
+  handleClickFavorites: '',
+  favorite: false,
 };
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import LoadingScreen from './LoadingScreen';
 
 class Album extends Component {
   state = {
     artistName: '',
     collectionName: '',
     musics: [],
+    loading: false,
   };
 
   async componentDidMount() {
@@ -16,25 +19,70 @@ class Album extends Component {
     const albumInfo = await getMusics(`${id}`);
     const firstElementRemoved = ([, ...y]) => y;
     const musicsInfo = firstElementRemoved(albumInfo);
+    const musicsInfoWithFavorite = musicsInfo.map((musicObj) => {
+      musicObj.favorite = false;
+      return musicObj;
+    });
     this.setState({
       artistName: albumInfo[0].artistName,
       collectionName: albumInfo[0].collectionName,
-      musics: musicsInfo,
+      musics: musicsInfoWithFavorite,
     });
   }
 
+  replaceMusic = (musics, music) => {
+    const vectorCopied = [...musics];
+    const index = musics.indexOf(music);
+    const musicCopied = { ...music };
+    musicCopied.favorite = !music.favorite;
+    vectorCopied[index] = musicCopied;
+    return vectorCopied;
+  };
+
+  handleClickFavorites = ({ target }) => {
+    const { musics } = this.state;
+    const music = musics.find((v) => v.trackId === Number(target.id));
+    if (target.checked) {
+      this.setState({
+        loading: true,
+        musics: this.replaceMusic(musics, music),
+      }, () => {
+        addSong(music)
+          .then(this.setState({ loading: false }));
+      });
+    } else {
+      this.setState({
+        loading: true,
+        musics: this.replaceMusic(musics, music),
+      }, () => {
+        removeSong(music)
+          .then(this.setState({ loading: false }));
+      });
+    }
+  };
+
   render() {
-    const { artistName, collectionName, musics } = this.state;
+    const { artistName, collectionName, musics, loading } = this.state;
+
+    if (loading) {
+      return <LoadingScreen />;
+    }
+
     return (
       <div data-testid="page-album">
         <Header />
         <h2 data-testid="album-name">{ collectionName }</h2>
         <h3 data-testid="artist-name">{ artistName }</h3>
         <ul>
-          {musics.map(({ trackName, previewUrl }) => {
-            const tn = trackName;
-            const pv = previewUrl;
-            const v = <MusicCard key={ tn } trackName={ tn } previewUrl={ pv } />;
+          {musics.map(({ trackName, previewUrl, trackId, favorite }) => {
+            const v = (<MusicCard
+              key={ trackName }
+              trackName={ trackName }
+              previewUrl={ previewUrl }
+              trackId={ trackId }
+              handleClickFavorites={ this.handleClickFavorites }
+              favorite={ favorite }
+            />);
             return <li key={ trackName }>{ v }</li>;
           })}
         </ul>
